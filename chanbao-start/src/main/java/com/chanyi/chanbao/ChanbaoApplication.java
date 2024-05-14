10e164a03dfbb7b757829ce902e5fe1bc92c4d4a
yijinchan <3146731710@qq.com>
2024-05-14 15:43:09 +0800

    Initial commit

---
+++ chanbao-start/src/main/java/com/chanyi/chanbao/ChanbaoApplication.java
@@ -0,0 +1,13 @@
+package com.chanyi.chanbao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanbaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChanbaoApplication.class, args);
+    }
+
+}
---
+++ chanbao-start/src/test/java/com/chanyi/chanbao/ChanbaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanyi.chanbao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChanbaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
